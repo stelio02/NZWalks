3028cf2cd54732718089f24116ebe3c176308581
stelio02 <spapa21@epoka.edu.al>
2024-11-10 17:50:49 +0100

    Summary

--- NZWalks.UI/Controllers/RegionsController.cs
+++ NZWalks.UI/Controllers/RegionsController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.UI.Models;
 using NZWalks.UI.Models.DTO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text;
 
 namespace NZWalks.UI.Controllers
 {
@@ -12,10 +16,11 @@ namespace NZWalks.UI.Controllers
             this.clientFactory = clientFactory;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
 
-            List<RegionDTO> response= new List<RegionDTO>();
+            List<RegionDTO> response = new List<RegionDTO>();
 
             try
             {
@@ -26,16 +31,109 @@ namespace NZWalks.UI.Controllers
 
                 httpResponeMessage.EnsureSuccessStatusCode();
 
-                 response.AddRange( await httpResponeMessage.Content.ReadFromJsonAsync<IEnumerable<RegionDTO>>());
+                response.AddRange(await httpResponeMessage.Content.ReadFromJsonAsync<IEnumerable<RegionDTO>>());
 
             }
             catch (Exception ex)
             {
                 //Log exception
-                
+
             }
 
             return View(response);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddRegionViewModel model)
+        {
+            var client = clientFactory.CreateClient();
+
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://localhost:7118/api/regions"),
+                Content = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json")
+            };
+
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var respose = await httpResponseMessage.Content.ReadFromJsonAsync<RegionDTO>();
+
+            if (respose is not null)
+            {
+                return RedirectToAction("Index", "Regions");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var client = clientFactory.CreateClient();
+
+            var response = await client.GetFromJsonAsync<RegionDTO>($"https://localhost:7118/api/regions/{id.ToString()}");
+
+            if (response is not null)
+            {
+                return View(response);
+            }
+
+            return View(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(RegionDTO request)
+        {
+            var client = clientFactory.CreateClient();
+
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Put,
+                RequestUri = new Uri($"https://localhost:7118/api/regions/{request.Id}"),
+                Content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json")
+            };
+
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var respose = await httpResponseMessage.Content.ReadFromJsonAsync<RegionDTO>();
+
+            if (respose is not null)
+            {
+                return RedirectToAction("Edit", "Regions");
+            }
+
+            return View();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(RegionDTO request)
+        {
+            try
+            {
+                var client = clientFactory.CreateClient();
+
+                var httpResponseMessage = await client.DeleteAsync($"https://localhost:7118/api/regions/{request.Id}");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                return RedirectToAction("Index", "Regions");
+            }
+            catch (Exception ex)
+            {
+                // Console
+            }
+
+            return View("Edit");
         }
     }
 }
